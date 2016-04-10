e9010eca852c0260d381db14c50aff1ad70f169b
Joao <jofisaes@hotmail.com>
2016-04-10 17:37:18 +0200

    SZ-N Setup for J2EE

---
+++ bisca-ui/app/app.component2.ts
@@ -0,0 +1,7 @@
+import {Component} from 'angular2/core';
+
+@Component({
+    selector: 'my-app2',
+    template: '<h1>BiscaJE2 game</h1>'
+})
+export class AppComponent2 { }
--- bisca-ui/app/main.ts
+++ bisca-ui/app/main.ts
@@ -1,4 +1,6 @@
 import {bootstrap}    from 'angular2/platform/browser';
 import {AppComponent} from './app.component';
+import {AppComponent2} from './app.component2';
 
 bootstrap(AppComponent);
+bootstrap(AppComponent2);
