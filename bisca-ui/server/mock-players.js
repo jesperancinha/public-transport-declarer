999d41cb38cf24e02598a53af315038a5aace019
Joao <jofisaes@hotmail.com>
2016-04-11 00:24:22 +0200

    SZ-N Adds one more player to game

--- bisca-ui/server/mock-players.js
+++ bisca-ui/server/mock-players.js
@@ -1,6 +1,18 @@
 exports.data = [
         {
             id: 1,
-            name: 'Joao'
+            name: 'Player 1',
+            cards: [
+                { cardNumber : 1 },
+                { cardNumber : 52 },
+                { cardNumber : 26 }
+            ]
+            ncards: 3
+        },
+        {
+            id: 2,
+            name: 'Player 2',
+            cards: [],
+            ncards: 2
         }
     ];
