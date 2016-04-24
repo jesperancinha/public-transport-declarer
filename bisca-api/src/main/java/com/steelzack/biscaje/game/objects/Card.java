2eb7c82704b45f01c290efa493e69053e90e666a
Joao <jofisaes@hotmail.com>
2016-04-24 14:24:46 +0200

    SZ-N Creation of decks test

--- bisca-api/src/main/java/com/steelzack/biscaje/game/objects/Card.java
+++ bisca-api/src/main/java/com/steelzack/biscaje/game/objects/Card.java
@@ -1,5 +1,6 @@
 package com.steelzack.biscaje.game.objects;
 
+import com.steelzack.biscaje.game.enums.CardType;
 import com.steelzack.biscaje.game.enums.SuitType;
 
 /**
@@ -13,4 +14,6 @@ public interface Card {
     Integer getOrderNumber();
 
     String getImage();
+
+    CardType getCardType();
 }
