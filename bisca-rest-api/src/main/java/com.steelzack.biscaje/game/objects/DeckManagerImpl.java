f2c10dfa0d5d1e85748ebdf46c5991663271bcb9
Joao <jofisaes@hotmail.com>
2016-04-24 15:34:49 +0200

    SZ-N Skeleton for creating full decks

--- bisca-rest-api/src/main/java/com.steelzack.biscaje/game/objects/DeckManagerImpl.java
+++ bisca-rest-api/src/main/java/com.steelzack.biscaje/game/objects/DeckManagerImpl.java
@@ -1,13 +1,22 @@
 package com.steelzack.biscaje.game.objects;
 
 import com.steelzack.biscaje.game.enums.DeckType;
+import com.steelzack.biscaje.game.enums.SuitType;
+
+import java.util.Map;
 
 /**
  * Created by joaofilipesabinoesperancinha on 17-04-16.
  */
 public class DeckManagerImpl implements DeckManager {
 
-    public DeckManagerImpl(DeckType deckType) {
+    final Map<SuitType, Suit> deckCards;
 
+    public DeckManagerImpl(DeckType deckType) {
+        deckCards = createAllCards(deckType);
+    }
+
+    private Map<SuitType,Suit> createAllCards(DeckType deckType) {
+        return null;
     }
 }
