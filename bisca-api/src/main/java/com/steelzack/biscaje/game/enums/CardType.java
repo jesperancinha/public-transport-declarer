a5e3e4ae6446d0ffda155f418eeb0f5888593d32
Joao <jofisaes@hotmail.com>
2016-04-24 14:14:51 +0200

    SZ-N Creates Suit for Decks

--- bisca-api/src/main/java/com/steelzack/biscaje/game/enums/CardType.java
+++ bisca-api/src/main/java/com/steelzack/biscaje/game/enums/CardType.java
@@ -1,35 +1,39 @@
 package com.steelzack.biscaje.game.enums;
 
+import lombok.Getter;
+import static com.steelzack.biscaje.game.enums.DeckType.PORTUGUESE;
+
 /**
  * Created by joaofilipesabinoesperancinha on 13-04-16.
  */
+@Getter
 public enum CardType {
-    NUMERIC_ACE_PT_(1, 11, DeckType.PORTUGUESE), //
-    NUMERIC_2_PT_(2, 0, DeckType.PORTUGUESE), //
-    NUMERIC_3_PT_(3, 0, DeckType.PORTUGUESE), //
-    NUMERIC_4_PT_(4, 0, DeckType.PORTUGUESE), //
-    NUMERIC_5_PT_(5, 0, DeckType.PORTUGUESE), //
-    NUMERIC_6_PT_(6, 0, DeckType.PORTUGUESE), //
-    NUMERIC_7_PT_(7, 10, DeckType.PORTUGUESE), //
-    NUMERIC_8_PT_(8, 0, DeckType.PORTUGUESE), //
-    NUMERIC_9_PT_(9, 0, DeckType.PORTUGUESE), //
-    NUMERIC_10_PT_(10, 0, DeckType.PORTUGUESE), //
-    NUMERIC_Q_PT_(11, 2, DeckType.PORTUGUESE), //
-    NUMERIC_J_PT_(12, 3, DeckType.PORTUGUESE), //
-    NUMERIC_K_PT_(13, 4, DeckType.PORTUGUESE); //
+    NUMERIC_ACE_PT_(1, 11, PORTUGUESE), //
+    NUMERIC_2_PT_(2, 0, PORTUGUESE), //
+    NUMERIC_3_PT_(3, 0, PORTUGUESE), //
+    NUMERIC_4_PT_(4, 0, PORTUGUESE), //
+    NUMERIC_5_PT_(5, 0, PORTUGUESE), //
+    NUMERIC_6_PT_(6, 0, PORTUGUESE), //
+    NUMERIC_7_PT_(7, 10, PORTUGUESE), //
+    NUMERIC_8_PT_(8, 0, PORTUGUESE), //
+    NUMERIC_9_PT_(9, 0, PORTUGUESE), //
+    NUMERIC_10_PT_(10, 0, PORTUGUESE), //
+    NUMERIC_Q_PT_(11, 2, PORTUGUESE), //
+    NUMERIC_J_PT_(12, 3, PORTUGUESE), //
+    NUMERIC_K_PT_(13, 4, PORTUGUESE); //
 
     private final int order;
-    private final int value;
-    private final DeckType portuguese;
+    private final int score;
+    private final DeckType deckType;
 
     CardType( //
               final int order, //
-              final int value, //
-              final DeckType portuguese //
+              final int score, //
+              final DeckType deckType //
     ) { //
 
         this.order = order;
-        this.value = value;
-        this.portuguese = portuguese;
+        this.score = score;
+        this.deckType = deckType;
     }
 }
--- bisca-api/src/main/java/com/steelzack/biscaje/game/objects/Suit.java
+++ bisca-api/src/main/java/com/steelzack/biscaje/game/objects/Suit.java
@@ -1,10 +1,13 @@
 package com.steelzack.biscaje.game.objects;
 
+import com.steelzack.biscaje.game.enums.DeckType;
 import com.steelzack.biscaje.game.enums.SuitType;
+
+import java.util.Map;
 
 /**
  * Created by joaofilipesabinoesperancinha on 23-04-16.
  */
 public interface Suit {
-    SuitType getSuit();
+    Map<Integer, Card> createCards(SuitType suitType, DeckType deckType);
 }
