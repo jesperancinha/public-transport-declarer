fe7e6c071c8339b40bf324797287e2403f84c2c0
Joao FSE <jofisaes@hotmail.com>
2016-08-10 15:57:44 +0200

    SZ-N Reconfigures lib folder

---
+++ bisca-rest-api/src/test/java/com/steelzack/biscaje/server/BiscaRestServerTest.java
@@ -0,0 +1,40 @@
+package com.steelzack.biscaje.server;
+//
+//import org.jboss.arquillian.container.test.api.Deployment;
+//import org.jboss.arquillian.junit.Arquillian;
+//import org.jboss.shrinkwrap.api.ShrinkWrap;
+//import org.jboss.shrinkwrap.api.asset.EmptyAsset;
+//import org.jboss.shrinkwrap.api.spec.JavaArchive;
+import org.junit.Test;
+//import org.junit.runner.RunWith;
+//
+//import javax.inject.Inject;
+
+/**
+ * Created by joaofilipesabinoesperancinha on 10-08-16.
+ */
+//@RunWith(Arquillian.class)
+public class BiscaRestServerTest {
+
+//    @Deployment
+//    public static JavaArchive createDeployment() {
+//        return ShrinkWrap.create(JavaArchive.class)
+//                .addClass(BiscaRestServer.class)
+//                .addAsManifestResource(EmptyAsset.INSTANCE, "beans.xml");
+//    }
+
+//    @Inject
+//    BiscaRestServer biscaRestServer;
+//
+
+    @Test
+    public void ping() throws Exception {
+//        String result = biscaRestServer.ping();
+    }
+
+    @Test
+    public void createNewUser() throws Exception {
+
+    }
+
+}
