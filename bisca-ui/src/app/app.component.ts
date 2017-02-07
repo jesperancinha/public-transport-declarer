688a76c3dd5af49b8fb599bf061c90793816d047
Joao Esperancinha <jofisaes@gmail.com>
2017-02-07 15:58:22 +0100

    running-version-systemjs

--- bisca-ui/src/app/app.component.ts
+++ bisca-ui/src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: 'bisca-title',
+  templateUrl: './src/app/app.component.html',
+  styleUrls: ['./src/app/app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  title = 'This is the main component for the Bisca-JE to start running with Angular 2. Congrats!';
 }
