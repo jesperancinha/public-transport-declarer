1bf1d72cf450f757790039a4b2b757bfb93f5b11
Joao Esperancinha <jofisaes@gmail.com>
2017-03-01 15:23:33 +0100

    working unit tests

--- bisca-ui/src/app/app.component.ts
+++ bisca-ui/src/app/app.component.ts
@@ -2,8 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'bisca-title',
-  templateUrl: './src/app/app.component.html',
-  styleUrls: ['./src/app/app.component.css']
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'This is the main component for the Bisca-JE to start running with Angular 2. Congrats!';
