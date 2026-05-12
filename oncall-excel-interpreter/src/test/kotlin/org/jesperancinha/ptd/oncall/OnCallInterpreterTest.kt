e093334b50d674b0ef15b69b9fa3adba4ac4a2c3
jesperancinha <jofisaes@gmail.com>
2026-05-12 14:13:53 +0200

    Tests and anonymous file

--- oncall-excel-interpreter/src/test/kotlin/org/jesperancinha/ptd/oncall/OnCallInterpreterTest.kt
+++ oncall-excel-interpreter/src/test/kotlin/org/jesperancinha/ptd/oncall/OnCallInterpreterTest.kt
@@ -1,33 +1,101 @@
 package org.jesperancinha.ptd.oncall
 
+import org.apache.poi.hssf.usermodel.HSSFWorkbook
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.Test
 import java.io.File
+import java.io.FileInputStream
 import java.time.Month
 
 class OnCallInterpreterTest {
 
+    val xltCalendarFile =
+        File(javaClass.getResource("/calendar.xlt")?.path ?: throw RuntimeException("Expected file not found!"))
+    val icsFile = File(
+        javaClass.getResource("/oncallschedule_test.ics")?.path ?: throw RuntimeException("Expected file not found!")
+    )
+
     @Test
     fun `should interpret ics and fill excel correctly for April 2026`() {
-        val icsFile = File("../oncallschedule_test.ics")
-        val xltFile = File("../test_calendar.xlt")
         val outputFile = File("target/oncall_april_2026.xls")
+        val expectedFile = File(
+            javaClass.getResource("/expected_oncall_april_2026.xls")?.path
+                ?: throw RuntimeException("Expected file not found!")
+        )
         outputFile.parentFile.mkdirs()
 
-        val interpreter = OnCallInterpreter(icsFile, xltFile, 2026, Month.APRIL)
+        val interpreter = OnCallInterpreter(icsFile, xltCalendarFile, 2026, Month.APRIL)
         interpreter.interpret(outputFile)
 
-        assert(outputFile.exists())
+        assertTrue(outputFile.exists())
         println("Output generated at: ${outputFile.absolutePath}")
+
+        verifyExcelFiles(outputFile, expectedFile)
+    }
+
+    private fun verifyExcelFiles(outputFile: File, expectedFile: File) {
+        val workbookActual = HSSFWorkbook(FileInputStream(outputFile))
+        val workbookExpected = HSSFWorkbook(FileInputStream(expectedFile))
+
+        val sheetActual = workbookActual.getSheetAt(0)
+        val sheetExpected = workbookExpected.getSheetAt(0)
+
+        assertEquals(sheetExpected.lastRowNum, sheetActual.lastRowNum)
+
+        for (i in 0..sheetActual.lastRowNum) {
+            val rowActual = sheetActual.getRow(i)
+            val rowExpected = sheetExpected.getRow(i)
+
+            if (rowActual == null || rowExpected == null) {
+                assertEquals(rowExpected, rowActual)
+                continue
+            }
+
+            assertEquals(rowExpected.lastCellNum, rowActual.lastCellNum)
+            for (j in 0 until rowActual.lastCellNum.toInt()) {
+                val cellActual = rowActual.getCell(j)
+                val cellExpected = rowExpected.getCell(j)
+
+                if (cellActual == null || cellExpected == null) {
+                    assertEquals(cellExpected, cellActual)
+                    continue
+                }
+
+                assertEquals(cellExpected.cellType, cellActual.cellType)
+                when (cellActual.cellType) {
+                    org.apache.poi.ss.usermodel.CellType.NUMERIC -> assertEquals(
+                        cellExpected.numericCellValue,
+                        cellActual.numericCellValue
+                    )
+
+                    org.apache.poi.ss.usermodel.CellType.STRING -> assertEquals(
+                        cellExpected.stringCellValue,
+                        cellActual.stringCellValue
+                    )
+
+                    org.apache.poi.ss.usermodel.CellType.BOOLEAN -> assertEquals(
+                        cellExpected.booleanCellValue,
+                        cellActual.booleanCellValue
+                    )
+
+                    org.apache.poi.ss.usermodel.CellType.FORMULA -> assertEquals(
+                        cellExpected.cellFormula,
+                        cellActual.cellFormula
+                    )
+
+                    else -> {}
+                }
+            }
+        }
     }
 
     @Test
     fun `should interpret ics and fill excel correctly for June 2026`() {
-        val icsFile = File("../oncallschedule_test.ics")
-        val xltFile = File("../test_calendar.xlt")
         val outputFile = File("target/oncall_june_2026.xls")
         outputFile.parentFile.mkdirs()
 
-        val interpreter = OnCallInterpreter(icsFile, xltFile, 2026, Month.JUNE)
+        val interpreter = OnCallInterpreter(icsFile, xltCalendarFile, 2026, Month.JUNE)
         interpreter.interpret(outputFile)
 
         assert(outputFile.exists())
@@ -36,12 +104,10 @@ class OnCallInterpreterTest {
 
     @Test
     fun `should interpret ics and fill excel correctly for October 2026`() {
-        val icsFile = File("../oncallschedule_test.ics")
-        val xltFile = File("../test_calendar.xlt")
         val outputFile = File("target/oncall_october_2026.xls")
         outputFile.parentFile.mkdirs()
 
-        val interpreter = OnCallInterpreter(icsFile, xltFile, 2026, Month.OCTOBER)
+        val interpreter = OnCallInterpreter(icsFile, xltCalendarFile, 2026, Month.OCTOBER)
         interpreter.interpret(outputFile)
 
         assert(outputFile.exists())
@@ -50,12 +116,10 @@ class OnCallInterpreterTest {
 
     @Test
     fun `should interpret ics and fill excel correctly for November 2026`() {
-        val icsFile = File("../oncallschedule_test.ics")
-        val xltFile = File("../test_calendar.xlt")
         val outputFile = File("target/oncall_november_2026.xls")
         outputFile.parentFile.mkdirs()
 
-        val interpreter = OnCallInterpreter(icsFile, xltFile, 2026, Month.NOVEMBER)
+        val interpreter = OnCallInterpreter(icsFile, xltCalendarFile, 2026, Month.NOVEMBER)
         interpreter.interpret(outputFile)
 
         assert(outputFile.exists())
